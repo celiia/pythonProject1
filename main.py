a50b8096e2e26c75b5a81dc2a87f590046e6ff85
celiia <celiacg999@gmail.com>
2021-07-05 09:09:19 +0200

    commit2

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Prueba de Celia')
+    print_hi('Prueba de Celia segundo cambio')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
